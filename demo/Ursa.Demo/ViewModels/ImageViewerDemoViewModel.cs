646905ab03e1c9ea53b2ff24886a5c9958742cf3
月影弓杯 <1424164638@qq.com>
2025-01-07 16:09:47 +0800

    添加 ImageViewer Strength 绑定测试

--- demo/Ursa.Demo/ViewModels/ImageViewerDemoViewModel.cs
+++ demo/Ursa.Demo/ViewModels/ImageViewerDemoViewModel.cs
@@ -1,8 +1,11 @@
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 
 namespace Ursa.Demo.ViewModels;
 
-public class ImageViewerDemoViewModel: ObservableObject
+public partial class ImageViewerDemoViewModel: ObservableObject
 {
-    
+    [ObservableProperty] private Stretch _stretch;
+    public Type StrethType { get; } = typeof(Stretch);
 }
